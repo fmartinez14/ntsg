4ba87db335bc0bbb32a39bd96e944d5b0174e2ea
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2018-10-13 23:17:42 -0600

    Create tagOverlay.py

---
+++ tagOverlay.py
@@ -0,0 +1,77 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class openPCAP(Gtk.ListBoxRow):
+    def __init__(self, data):
+        super(Gtk.ListBoxRow, self).__init__()
+        self.data = data
+        self.add(Gtk.Label(data))
+
+class openPCAPwindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="PCAP")
+        self.set_border_width(10)
+
+        box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(box_outer)
+
+        tagDescription = Gtk.Label("Launch Description")
+        tagDescription.set_text("Tag Overlay")
+        box_outer.pack_start(tagDescription, False, False, 0)
+
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        box_outer.pack_start(listbox, True, True, 0)
+
+        row = Gtk.ListBoxRow()
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        row.add(hbox)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        hbox.pack_start(vbox, True, True, 0)
+
+        tagLabel = Gtk.Label("Tag Name   ", xalign=0)
+        vbox.pack_start(tagLabel, False, True, 0)
+
+        tagEntry = Gtk.Entry()
+        tagEntry.props.valign = Gtk.Align.CENTER
+        hbox.pack_start(tagEntry, True, True, 0)
+
+        listbox.add(row)
+
+        row = Gtk.ListBoxRow()
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        row.add(hbox)
+        tagFieldlabel = Gtk.Label("Tag Field      ", xalign=0)
+        tagFieldEntry = Gtk.Entry()
+        hbox.pack_start(tagFieldlabel, False, True, 0)
+        hbox.pack_start(tagFieldEntry, True, True, 0)
+
+        listbox.add(row)
+
+        row = Gtk.ListBoxRow()
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        row.add(hbox)
+        tagDescriptionlabel = Gtk.Label("Description", xalign=0)
+        tagDescriptionEntry = Gtk.Entry()
+        hbox.pack_start(tagDescriptionlabel, False, True, 0)
+        hbox.pack_start(tagDescriptionEntry, True, True, 0)
+
+        listbox.add(row)
+
+        row = Gtk.ListBoxRow()
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing = 50)
+        row.add(hbox)
+        SaveButton = Gtk.Button(label="Save")
+        CancelButton = Gtk.Button(label="Cancel")
+        hbox.pack_start(SaveButton, True, True, 0)
+        hbox.pack_start(CancelButton, True, True, 0)
+
+        listbox.add(row)
+
+
+win = openPCAPwindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
