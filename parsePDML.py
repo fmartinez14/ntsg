078b4f45ea015d431c1482d1baec8e65d8410a87
Fernando <fmartinez14@miners.utep.edu>
2018-11-21 13:13:31 -0700

    commenting parse pdml file.

--- parsePDML.py
+++ parsePDML.py
@@ -4,21 +4,15 @@ from Protocol import Protocol
 from Field import Field
 from pprint import pprint
 
-def parsePDML():
-
+def parsePDML(): #Dont use me , parses pdml but does not create objects inside the packets.
     myFile = open("projectPCAP.pdml", 'r')
-
     # create element tree object
     tree = ET.parse(myFile)
-
     # get root element
     root = tree.getroot()
-
     # create empty list for objects
     Packets = {}
     Protocols = {}
-
-
     for item in root.findall('packet'):
         for child in item:
             Packets[item] = child
@@ -26,11 +20,19 @@ def parsePDML():
             for fields in child.findall('field'):
                 FieldValues.append(fields)
             Protocols[child] = FieldValues
-
     return Packets
 
-def makePackets():
+def printTree(leTree): #Dont use me, used for debugging the extraction and creation w/o objects.
+    for item in tester:
+        print("-------Packet\n")
+        for protocol in item:
+            print("-------------Protocol" + str(protocol.attrib) + "\n")
+            for field in protocol:
+                print("------------Field:" + str(field.attrib))
+            print("\n")
 
+
+def makePackets(): #Creates a Packet Dictionary object, where the keys are Packet objects and the values are Protocol objects. Furthermore, the Protocol Dictionary that is also returned contains the protocols as keys and a list of fields as a value.
     myFile = open("projectPCAP.pdml", 'r')
 
     # create element tree object
@@ -60,7 +62,7 @@ def makePackets():
     ListMe = [Packets,Protocols]
     return ListMe
 
-def printPackets(Packets,Protocols):
+def printPackets(Packets,Protocols): #Prints the resulting packet/protocol/field name.
     for key,value in Packets.iteritems():
         print("----Protocol: \n")
         # print(str(type(Protocols[value])) + " Haaa")
@@ -69,14 +71,6 @@ def printPackets(Packets,Protocols):
             for myField in fields:
                 print(myField.name)
 
-def printTree(leTree):
-    for item in tester:
-        print("-------Packet\n")
-        for protocol in item:
-            print("-------------Protocol" + str(protocol.attrib) + "\n")
-            for field in protocol:
-                print("------------Field:" + str(field.attrib))
-            print("\n")
 
 
 # tester = parsePDML()
