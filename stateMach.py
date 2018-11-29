e93c64498c99b05f99ba0896be4eac2979ac395c
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2018-11-29 14:35:54 -0700

    Added add and edit edge

--- stateMach.py
+++ stateMach.py
@@ -16,7 +16,7 @@ class stateMachine:
 		self.node = []
 
 		for i in range(self.numNodes):
-			self.node.append(pydot.Node("Node %d" % i, rank="same"))
+			self.node.append(pydot.Node("Node %d" % (i+1), rank="same"))
 			self.graph.add_node(self.node[i])
 
 		for i in range(self.numNodes):
@@ -32,8 +32,19 @@ class stateMachine:
 		self.graph.del_edge(self.node[x], self.node[y])
 		self.graph.write_png('stateMachine.png')
 
+	def addEdge(self, x, y):
+		self.graph.add_edge(pydot.Edge(self.node[x], self.node[y]))
+		self.graph.write_png('stateMachine.png')
+
+	def editEdge(self, x, y, z):
+		self.graph.del_edge(self.node[x], self.node[y])
+		self.graph.add_edge(pydot.Edge(self.node[x], self.node[z]))
+		self.graph.write_png('stateMachine.png')
 
 machine = stateMachine()
-machine.numNodes = 3
+machine.numNodes = 4
 machine.createMachine()
-machine.deleteEdge(1,2)
+machine.deleteEdge(3,0)
+machine.addEdge(1,1)
+machine.addEdge(1,1)
+machine.editEdge(1,1,2)
