b53c442c6901d21cd9ff8639b2c18949b895769f
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2018-11-28 22:21:39 -0700

    Made it object oriented

--- stateMach.py
+++ stateMach.py
@@ -1,22 +1,32 @@
 import pydot
 
-# this time, in graph_type we specify we want a DIrected GRAPH
-graph = pydot.Dot(graph_type='digraph', rankdir = "LR")
 
-graphlegend = pydot.Cluster(graph_name="NTSG", label="NTSG", rankdir="TB")
+class stateMachine:
 
-numNodes = 5
+	def __init(self):
+		self.numNodes = 0
 
-node = []
+	def createMachine(self):
+		graph = pydot.Dot(graph_type='digraph', rankdir = "LR")
 
-for i in range(numNodes):
-	node.append(pydot.Node("Node %d" % i, rank="same"))
-	graph.add_node(node[i])
+		graphlegend = pydot.Cluster(graph_name="NTSG", label="NTSG", rankdir="TB")
 
-for i in range(numNodes):
-	if(i < (numNodes-1)):
-		graph.add_edge(pydot.Edge(node[i], node[i+1]))
-	else:
-		graph.add_edge(pydot.Edge(node[i], node[0]))
+		node = []
 
-graph.write_png('example3_graph.png')
+		for i in range(self.numNodes):
+			node.append(pydot.Node("Node %d" % i, rank="same"))
+			graph.add_node(node[i])
+
+		for i in range(self.numNodes):
+			if(i < (self.numNodes-1)):
+				graph.add_edge(pydot.Edge(node[i], node[i+1]))
+			else:
+				graph.add_edge(pydot.Edge(node[i], node[0]))
+
+		graph.del_edge(node[2], node[3])
+
+		graph.write_png('example3_graph.png')
+
+machine = stateMachine()
+machine.numNodes = 6
+machine.createMachine()
